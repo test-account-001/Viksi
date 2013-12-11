3ce3a2113553183426d2ae963fd7983466f5ed55
Antony <antony.jekov@gmail.com>
2013-12-11 23:22:24 +0200

    Changed serialization of the BrokerLoggedInViewModel.cs

--- trunk/server/Viksi.Web.Api/ViewModels/BrokerLoggedInViewModel.cs
+++ trunk/server/Viksi.Web.Api/ViewModels/BrokerLoggedInViewModel.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Viksi.Web.Api.ViewModels
+﻿namespace Viksi.Web.Api.ViewModels
 {
+    using System.Runtime.Serialization;
+    
+    [DataContract]
     public class BrokerLoggedInViewModel
     {
+        [DataMember(Name="email")]
         public string Email { get; set; }
 
+        [DataMember(Name = "firstName")]
         public string FirstName { get; set; }
 
+        [DataMember(Name = "LastName")]
         public string LastName { get; set; }
 
+        [DataMember(Name = "sessionKey")]
         public string SessionKey { get; set; }
     }
 }
